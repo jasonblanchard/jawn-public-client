d77c20e1d65147e880c9ccc8e0f21ce25c88dd5d
Jason Blanchard <jas.blanchard@gmail.com>
2019-09-02 11:04:02 -0400

    Added CI stuff

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-source $(dirname $0)/config
-TAG="$(git rev-parse HEAD)"
-
-docker-compose build
-docker tag ${IMAGE} ${IMAGE}:${TAG}
--- scripts/push.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-source $(dirname $0)/config
-TAG="$(git rev-parse HEAD)"
-docker push ${IMAGE}:${TAG}
